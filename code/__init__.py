7f2058bdda4d69651557f01d111d9f40bec0ea2a
Francesco Corda <35812424+francescocorda@users.noreply.github.com>
2020-04-02 14:27:41 +0200

    Update.  - added instructions.txt file  - first attempt to interpolate some points

--- code/__init__.py
+++ code/__init__.py
@@ -1,2 +1,24 @@
+import numpy as np
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+
 if __name__ == '__main__':
-    print('hello')
+    x = np.linspace(0, 10, num=11, endpoint=True)
+    y = np.exp(-x ** 2 / 9)
+    f1 = interp1d(x, y, kind='linear')
+    f2 = interp1d(x, y, kind='quadratic')
+    f3 = interp1d(x, y, kind='cubic')
+    f4 = interp1d(x, y, kind='nearest')
+    f5 = interp1d(x, y, kind='previous')
+    f6 = interp1d(x, y, kind='next')
+
+    x_new = np.linspace(0, 10, num=41, endpoint=True)
+
+    plt.plot(x, y, 'o', x_new, f1(x_new), '-', x_new, f2(x_new), '--', x_new, f3(x_new), ':')
+    plt.legend(['data', 'linear', 'quadratic', 'cubic'], loc='best')
+    plt.show()
+
+    plt.plot(x, y, 'o')
+    plt.plot(x_new, f4(x_new), '-', x_new, f5(x_new), '--', x_new, f6(x_new), ':')
+    plt.legend(['data', 'nearest', 'previous', 'next'], loc='best')
+    plt.show()
