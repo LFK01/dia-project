6441157ab710f0af1f563917918b3111fe9d6226
LFK01 <37867917+LFK01@users.noreply.github.com>
2020-04-02 16:16:56 +0200

    Update:  - changed example

--- code/mab/example.py
+++ code/mab/example.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from code.mab.Non_Stationary_Environment import *
 from code.mab.TS_Learner import *
 from code.mab.SWTS_Learner import *
+from code.mab.greedy_learner import *
 
 
 if __name__ == '__main__':
@@ -16,7 +17,7 @@ if __name__ == '__main__':
     y3 = np.exp(-x3 ** 2 / 9)
 
     p1 = np.array([y1[0:4], y1[4:8], y1[8:12], y1[12:16]])
-    p2= np.array([y2[0:4], y2[4:8], y2[8:12], y2[12:16]])
+    p2 = np.array([y2[0:4], y2[4:8], y2[8:12], y2[12:16]])
     p3 = np.array([y3[0:4], y3[4:8], y3[8:12], y3[12:16]])
 
     n_arms = 4
@@ -26,55 +27,31 @@ if __name__ == '__main__':
 
     n_experiments = 100
     ts_rewards_per_experiment = []
-    swts_rewards_per_experiment = []
-    window_size = int(np.sqrt(T))
+    gr_rewards_per_experiment = []
 
     for e in range(0, n_experiments):
-        ts_env = Non_Stationary_Environment(n_arms, probabilities=p, horizon=T)
-        ts_learner = TS_Learner(n_arms)
-
-        swts_env = Non_Stationary_Environment(n_arms=n_arms, probabilities=p, horizon=T)
-        swts_learner = SWTS_Learner(n_arms, window_size)
-
+        subcampaign1_environment = Environment(n_arms=n_arms, probabilities=p1)
+        env = Environment(n_arms=n_arms, probabilities=p)
+        ts_learner = TS_Learner(n_arms=n_arms)
+        gr_learner = Greedy_Learner(n_arms=n_arms)
         for t in range(0, T):
+            # Thompson Sampling Learner
             pulled_arm = ts_learner.pull_arm()
-            reward = ts_env.round(pulled_arm)
+            reward = env.round(pulled_arm)
             ts_learner.update(pulled_arm, reward)
 
-            pulled_arm = swts_learner.pull_arm()
-            reward = swts_env.round(pulled_arm)
-            swts_learner.update(pulled_arm, reward)
+            # Greedy Learner
+            pulled_arm = gr_learner.pull_arm()
+            reward = env.round(pulled_arm)
+            gr_learner.update(pulled_arm, reward)
 
         ts_rewards_per_experiment.append(ts_learner.collected_rewards)
-        swts_rewards_per_experiment.append(swts_learner.collected_rewards)
+        gr_rewards_per_experiment.append(gr_learner.collected_rewards)
 
-    ts_instantaneous_regret = np.zeros(T)
-    swts_instantaneous_regret = np.zeros(T)
-    n_phases = len(p)
-    phases_len = int(T / n_phases)
-    opt_per_phases = p.max(axis=1)
-    optimum_per_round = np.zeros(T)
-
-    for i in range(0, n_phases):
-        optimum_per_round[i * phases_len: (i + 1) * phases_len] = opt_per_phases[i]
-        ts_instantaneous_regret[i * phases_len: (i + 1) * phases_len] = opt_per_phases[i] - np.mean(
-            ts_rewards_per_experiment, axis=0)[i * phases_len:(i + 1) * phases_len]
-        swts_instantaneous_regret[i * phases_len: (i + 1) * phases_len] = opt_per_phases[i] - np.mean(
-            swts_rewards_per_experiment, axis=0)[i * phases_len:(i + 1) * phases_len]
-
-    plt.figure(0)
-    plt.ylabel("Reward")
-    plt.xlabel("t")
-    plt.plot(np.mean(ts_rewards_per_experiment, axis=0), 'r')
-    plt.plot(np.mean(swts_rewards_per_experiment, axis=0), 'b')
-    plt.plot(optimum_per_round, '--k')
-    plt.legend(["TS", "SW-TS", "Optimum"])
-    plt.show()
-
-    plt.figure(1)
-    plt.ylabel("Regret")
-    plt.xlabel("t")
-    plt.plot(np.cumsum(ts_instantaneous_regret), 'r')
-    plt.plot(np.cumsum(swts_instantaneous_regret), 'b')
-    plt.legend(["TS", "SW-TS"])
-    plt.show()
+plt.figure(0)
+plt.ylabel("Regret")
+plt.xlabel("t")
+plt.plot(np.cumsum(np.mean(opt - ts_rewards_per_experiment,axis=0)), 'r')
+plt.plot(np.cumsum(np.mean(opt - gr_rewards_per_experiment,axis=0)),'g')
+plt.legend(["TS","Greedy"])
+plt.show()
