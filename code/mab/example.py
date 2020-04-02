8356cf830fa9f24223c8ea7b0f7b0ae94c6503f5
mattiabaca <mattia.bacarella@mail.polimi.it>
2020-04-02 15:57:57 +0200

    Added 3 classes

--- code/mab/example.py
+++ code/mab/example.py
@@ -6,13 +6,18 @@ from code.mab.SWTS_Learner import *
 
 
 if __name__ == '__main__':
-    x = np.linspace(0, 10, num=16, endpoint=True)
-    y = np.exp(-x ** 2 / 9)
 
+    x1 = np.linspace(0, 10, num=16, endpoint=True)
+    x2 = np.linspace(0, 10, num=16, endpoint=True)
+    x3 = np.linspace(0, 10, num=16, endpoint=True)
 
+    y1 = np.exp(-x1 ** 2 / 9)
+    y2 = np.exp(-x2 ** 2 / 9)
+    y3 = np.exp(-x3 ** 2 / 9)
 
-    p = np.array([y[0:4], y[4:8], y[8:12], y[12:16]])
-    print(p)
+    p1 = np.array([y1[0:4], y1[4:8], y1[8:12], y1[12:16]])
+    p2= np.array([y2[0:4], y2[4:8], y2[8:12], y2[12:16]])
+    p3 = np.array([y3[0:4], y3[4:8], y3[8:12], y3[12:16]])
 
     n_arms = 4
     # p = np.array([[0.15, 0.1, 0.2, 0.35], [0.35, 0.32, 0.2, 0.35], [0.5, 0.1, 0.1, 0.15], [0.8, 0.32, 0.1, 0.15]])
