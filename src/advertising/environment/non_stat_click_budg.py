65166a762092d06fe71d8c93da98560a13e5956e
luca-massini <37840272+Lucagibson96@users.noreply.github.com>
2020-04-26 19:28:48 +0200

    fixed wrong indentation in the non stat. env. constructor

--- src/advertising/environment/non_stat_click_budg.py
+++ src/advertising/environment/non_stat_click_budg.py
@@ -5,15 +5,17 @@ from scipy import interpolate
 
 class non_stat_click_env:
 
+    # CONSTRUCTOR OF THE CLASS
     # budgets is a matrix (number of phases x number of arms), phases length is an array in which the i-th number is the
     # length of the i+1 th phase. x_values is an matrix and y_values is also a matrix and they are used to compute
     # the functions for each phase interpolating the points. The interpolation is done interpolating the corresponding
     # x_values and y_values row
     def __init__(self, phases_length, x_values, y_values, sigma, budgets_matrix):
-        self.n_arms = len(y_values)
-        self.n_phases = len(phases_length)
 
-        self.functions = [interpolate.interp1d(x_values[index], y_values[index]) for index in range(0,self.n_phases)]
+        # number of phases
+        self.n_phases = len(phases_length)
+        # Functions of each abrupt phases obtained through interpolation
+        self.functions = [interpolate.interp1d(x_values[index], y_values[index]) for index in range(0, self.n_phases)]
         for phases_index in range(0, self.n_phases):
 
             # plot the phase_index+1 function
@@ -21,7 +23,7 @@ class non_stat_click_env:
             plt.ylabel("Rewards")
             plt.xlabel("arms")
             plt.plot(x_values[phases_index], y_values[phases_index], 'r')
-            plt.legend(["Environment function"])
+            plt.legend(["Environment function of the abrupt phase"+str(phases_index+1)])
             plt.show()
 
             # An array containing the times in which each phase will change
@@ -29,18 +31,17 @@ class non_stat_click_env:
             # the vector), the second from 3  (second position of the array) the third from 6 (the third position) and so on
             self.change_phases_time = np.zeros(self.n_phases)
 
-            # compute the array of time instants in which a phase change
-            for phase_length_index in reversed(range(0, self.n_phases)):
-                for index in reversed(range(0, phase_length_index)):
-                    self.change_phases_time[phase_length_index] = self.change_phases_time[phase_length_index] + \
+        # compute the array of time instants in which a phase change
+        for phase_length_index in reversed(range(0, self.n_phases)):
+            for index in reversed(range(0, phase_length_index)):
+                self.change_phases_time[phase_length_index] = self.change_phases_time[phase_length_index] + \
                                                                   phases_length[index]
-            # compute the sigma array
-            self.sigmas = np.ones(len(budgets_matrix[0])) * sigma
-            print(self.sigmas)
-            # compute the array of means. Each position is the mean of that phase
-            self.means = [self.functions[i](budgets_matrix[i]) for i in range(0, self.n_phases)]
-            print(self.means)
+        # compute the sigma array
+        self.sigmas = np.ones(len(budgets_matrix[0])) * sigma
+        # compute the array of means. Each position is the mean of that phase
+        self.means = [self.functions[i](budgets_matrix[i]) for i in range(0, self.n_phases)]
 
+    # FUNCTION: ROUND
     # Given the current time it selects the right environment and it returns the right value
     def round(self, pulled_arm, current_t):
         environmentIndex = 0
@@ -55,12 +56,12 @@ class non_stat_click_env:
         return np.maximum(0, np.random.normal(self.means[environmentIndex][pulled_arm], self.sigmas[pulled_arm]))
 
 
-# main used just for testing just to see if it works. Just 4 phases with different lengths and every function is a
-# linear function.
+# main used just for testing just to see if it works. Just 4 phases with different lengths and every function is
+# linear.
 if __name__ == "__main__":
     phases_l = [1, 2, 3, 4]
     x_val = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10] for i in range(0, 4)]
     y_val = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10] for i in range(0, 4)]
     budgets = np.array([[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]])
-    obj = non_stat_click_env(phases_l, x_val, y_val, 10, budgets)
-    print(obj.round(2,2))
+    obj = non_stat_click_env(phases_l, x_val, y_val, 1, budgets)
+    print(obj.round(2, 2))
