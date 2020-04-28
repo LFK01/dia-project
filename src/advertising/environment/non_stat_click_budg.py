b16d40091fe7cb8178d83b412d6e08dd75f3e891
luca-massini <37840272+Lucagibson96@users.noreply.github.com>
2020-04-28 11:23:42 +0200

    update: integration of the new environment and so added related functionalities to sw_budget_comparison

--- src/advertising/environment/non_stat_click_budg.py
+++ src/advertising/environment/non_stat_click_budg.py
@@ -11,31 +11,29 @@ class non_stat_click_env:
     # the functions for each phase interpolating the points. The interpolation is done interpolating the corresponding
     # x_values and y_values row
     def __init__(self, phases_length, x_values, y_values, sigma, budgets_matrix):
-
         # number of phases
         self.n_phases = len(phases_length)
         # Functions of each abrupt phases obtained through interpolation
         self.functions = [interpolate.interp1d(x_values[index], y_values[index]) for index in range(0, self.n_phases)]
         for phases_index in range(0, self.n_phases):
-
             # plot the phase_index+1 function
             plt.figure(phases_index)
             plt.ylabel("Rewards")
             plt.xlabel("arms")
             plt.plot(x_values[phases_index], y_values[phases_index], 'r')
-            plt.legend(["Environment function of the abrupt phase"+str(phases_index+1)])
+            plt.legend(["Environment function of the abrupt phase" + str(phases_index + 1)])
             plt.show()
 
-            # An array containing the times in which each phase will change
-            # for example if change_phases_time= [0,3,6,9] this means that the first phase will be from 0 (first position of
-            # the vector), the second from 3  (second position of the array) the third from 6 (the third position) and so on
-            self.change_phases_time = np.zeros(self.n_phases)
+        # An array containing the times in which each phase will change
+        # for example if change_phases_time= [0,3,6,9] this means that the first phase will be from 0 (first position of
+        # the vector), the second from 3  (second position of the array) the third from 6 (the third position) and so on
+        self.change_phases_time = np.zeros(self.n_phases)
 
         # compute the array of time instants in which a phase change
         for phase_length_index in reversed(range(0, self.n_phases)):
             for index in reversed(range(0, phase_length_index)):
                 self.change_phases_time[phase_length_index] = self.change_phases_time[phase_length_index] + \
-                                                                  phases_length[index]
+                                                              phases_length[index]
         # compute the sigma array
         self.sigmas = np.ones(len(budgets_matrix[0])) * sigma
         # compute the array of means. Each position is the mean of that phase
@@ -60,8 +58,8 @@ class non_stat_click_env:
 # linear.
 if __name__ == "__main__":
     phases_l = [1, 2, 3, 4]
-    x_val = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10] for i in range(0, 4)]
-    y_val = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10] for i in range(0, 4)]
-    budgets = np.array([[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]])
+    x_val = [np.linspace(0, 4, 10) for i in range(0, 4)]
+    y_val = [[1, 1.3, 1.7, 2, 2.5, 3.2, 3.9, 4.8, 4.9, 5.2] for i in range(0, 4)]
+    budgets = [np.array([1, 2, 3, 4]) for i in range(0, 4)]
     obj = non_stat_click_env(phases_l, x_val, y_val, 1, budgets)
     print(obj.round(2, 2))
--- src/advertising/sw_budget_comparison.py
+++ src/advertising/sw_budget_comparison.py
@@ -1,13 +1,12 @@
 import time
 import matplotlib.pyplot as plt
-from src.advertising.environment.click_budget import *
 from src.advertising.learner.gpts_learner import *
 from src.advertising.solver.knapsack import *
+from src.advertising.environment.non_stat_click_budg import *
 from tqdm import tqdm
 
 # Parameters initialization
 subcampaign = [0, 1, 2]
-
 min_budget = 0.0
 max_budget = 1.0
 n_arms = 21
@@ -17,22 +16,40 @@ sigma = 10
 # number of phases
 n_phases = 3
 # Time horizon multiple of the number of phases
-T = n_phases * 300
+T = n_phases * 100
 # Window size proportional to the square root of T and always integer
-window_size = int(np.sqrt(T) * 4.5)
+window_size = int(np.sqrt(T) * 3.4)
 # Number of experiments
-n_experiments = 5
+n_experiments = 2
+
+# BUILD OF THE 3 ENVIRONMENTS. ONE FOR EACH SUBCAMPAIGN. The single environment is not stationary
+# define the values of the x
+x_values = [np.linspace(min_budget, max_budget, n_arms) for i in range(0, n_phases)]
+# The values of the y for each phase
+y_values = [[0, 3, 6, 15, 36, 87, 136, 183, 223, 258, 288, 313, 333, 348, 358, 364, 366, 367, 367, 367, 367],
+            [0, 3, 6, 15, 36, 87, 88, 95, 112, 124, 135, 145, 145, 175, 200, 230, 250, 270, 300, 340, 367],
+            [0, 3, 6, 15, 49, 100, 140, 170, 180, 182, 183, 183, 183, 183, 210, 240, 280, 330, 343, 355, 367]]
+# define the length of each phase. The first is T/3 -10, the second T/3 and so on
+phase_length = [T / 3 - 10, T / 3, T / 3 + 10]
+# define the budgets
+budget_matrix = [daily_budget for i in range(0, n_phases)]
+# define 3 non stationary environment, one for each subcampaign
+environment_first_subcampaign = non_stat_click_env(phase_length, x_values, y_values, sigma, budget_matrix)
+environment_second_subcampaign = environment_first_subcampaign
+environment_third_subcampaign = environment_first_subcampaign
+env = [environment_first_subcampaign, environment_second_subcampaign, environment_third_subcampaign]
+# END
 
 collected_rewards_per_experiments = []
 sw_collected_rewards_per_experiments = []
 opt_per_phases = [0, 0, 0]
-functionType = [[0, 1, 2], [2, 2, 0], [1, 0, 1]]
 
 budgets = []
 for n in subcampaign:
     for b in daily_budget:
         budgets.append(b)
 
+# start of the experiments
 for e in tqdm(range(0, n_experiments), desc="Experiment processed", unit="exp"):
     # Initialize the environment, learner and click for each experiment
     gpts_learner = []
@@ -46,17 +63,6 @@ for e in tqdm(range(0, n_experiments), desc="Experiment processed", unit="exp"):
 
     # For each t in the time horizon, run the GP_TS algorithm
     for t in range(0, T):
-        # For every phase, reinitialize the environments
-        if t % (T / n_phases) == 0:
-            i = int(t / (T / n_phases))
-            env = []
-
-            # Create new environment based on phase
-            for s in subcampaign:
-                env.append(
-                    ClickBudget(s, budgets=daily_budget, sigma=sigma, max_value=150 * (i + 1),
-                                functionType=functionType[i][s]))
-
         total_subcampaign_combination = []
         sw_total_subcampaign_combination = []
         for s in subcampaign:
@@ -74,8 +80,8 @@ for e in tqdm(range(0, n_experiments), desc="Experiment processed", unit="exp"):
         total_clicks = 0
         sw_total_clicks = 0
         for s in subcampaign:
-            reward = env[s].round(superarm[s])
-            sw_reward = env[s].round(sw_superarm[s])
+            reward = env[s].round(superarm[s], t)
+            sw_reward = env[s].round(sw_superarm[s], t)
             total_clicks += reward
             sw_total_clicks += sw_reward
             gpts_learner[s].update(superarm[s], reward)
@@ -89,43 +95,46 @@ for e in tqdm(range(0, n_experiments), desc="Experiment processed", unit="exp"):
     sw_collected_rewards_per_experiments.append(sw_total_clicks_per_t)
     time.sleep(0.01)
 
+# computation of the optimal budget combination for each abrupt phase
 ts_instantaneous_regret = np.zeros(T)
 swts_instantaneous_regret = np.zeros(T)
-phases_len = int(T / n_phases)
 optimum_per_round = np.zeros(T)
-
-for i in range(0, 3):
-    env = []
+total_optimal_combination = []
+for p in range(0, n_phases):
     total_optimal_combination = []
-
-    for s in subcampaign:
-        env.append(
-            ClickBudget(s, budgets=daily_budget, sigma=sigma, max_value=150 * (i + 1),
-                        functionType=functionType[i][s]))
-        
-        value = []
+    for i in subcampaign:
         for idx in range(0, n_arms):
-            total_optimal_combination.append(env[s].means[idx])
-            value.append(env[s].means[idx])
-        plt.figure(0)
-        plt.ylabel("Rewards")
-        plt.xlabel("arms")
-        plt.plot(value, 'r')
-        plt.legend(["Environment", ])
-        plt.show()
-
+            total_optimal_combination.append(env[i].means[p][idx])
     optimal_reward = Knapsack(total_optimal_combination, budgets, n_arms).solve()
     for s in subcampaign:
-        opt_per_phases[i] += env[s].means[optimal_reward[s]]
+        opt_per_phases[p] += env[s].means[p][optimal_reward[s]]
 
+times_of_change = env[1].change_phases_time.tolist()
+times_of_change = [int(i) for i in times_of_change]
+phase_length = [int(i) for i in phase_length]
+print(times_of_change)
 # For each phase, calculate the regret accordingly to the optimum in this particular phase
-for i in range(0, n_phases):
-    optimum_per_round[i * phases_len: (i + 1) * phases_len] = opt_per_phases[i]
-    ts_instantaneous_regret[i * phases_len: (i + 1) * phases_len] = opt_per_phases[i] - np.mean(
-        collected_rewards_per_experiments, axis=0)[i * phases_len:(i + 1) * phases_len]
-    swts_instantaneous_regret[i * phases_len: (i + 1) * phases_len] = opt_per_phases[i] - np.mean(
-        sw_collected_rewards_per_experiments, axis=0)[i * phases_len:(i + 1) * phases_len]
+for i in range(0, n_phases - 1):
+    optimum_per_round[times_of_change[i]: times_of_change[i + 1]] = opt_per_phases[i]
+    ts_instantaneous_regret[times_of_change[i]: times_of_change[i + 1]] = opt_per_phases[i] - np.mean(
+        collected_rewards_per_experiments, axis=0)[times_of_change[i]: times_of_change[i + 1]]
+    swts_instantaneous_regret[times_of_change[i]: times_of_change[i + 1]] = opt_per_phases[i] - np.mean(
+        sw_collected_rewards_per_experiments, axis=0)[times_of_change[i]: times_of_change[i + 1]]
 
+# the last iteration is done out of the cycle and manually. TODO: FIND A WAY TO PUT THESE LINES INSIDE THE CYCLE ABOVE
+optimum_per_round[times_of_change[n_phases - 1]: phase_length[n_phases - 1] + times_of_change[n_phases - 1]] = \
+opt_per_phases[n_phases - 1]
+ts_instantaneous_regret[times_of_change[n_phases - 1]: phase_length[n_phases - 1] + times_of_change[n_phases - 1]] \
+    = opt_per_phases[n_phases - 1] - np.mean(collected_rewards_per_experiments, axis=0)[
+                                     times_of_change[n_phases - 1]: phase_length[n_phases - 1] + times_of_change[
+                                         n_phases - 1]]
+swts_instantaneous_regret[times_of_change[n_phases - 1]: phase_length[n_phases - 1] + times_of_change[n_phases - 1]] = \
+opt_per_phases[
+    n_phases - 1] - np.mean(
+    sw_collected_rewards_per_experiments, axis=0)[
+                    times_of_change[n_phases - 1]: phase_length[n_phases - 1] + times_of_change[n_phases - 1]]
+
+# plot the results
 print("Opt: ")
 np.set_printoptions(precision=3)
 print(opt_per_phases)
